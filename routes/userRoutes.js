fc9a0ae228d0620029dcfb10b41eea472b12f558
LindRagnarsson <leyvanunodaniel@gmail.com>
2025-11-24 01:35:12 -0700

    Add user management and password update API routes.

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -56,7 +56,12 @@ const userRoutes = [
     options: {
       description: 'Update user by id',
       tags: ['api', 'users'],
-      auth: 'jwt'
+      auth: 'jwt',
+      validate: {
+        params: Joi.object({
+          id: Joi.string().required().description('the user id')
+        })
+      }
     },
     handler: updateUser
   },
@@ -66,7 +71,12 @@ const userRoutes = [
     options: {
       description: 'Delete user by id',
       tags: ['api', 'users'],
-      auth: 'jwt'
+      auth: 'jwt',
+      validate: {
+        params: Joi.object({
+          id: Joi.string().required().description('the user id')
+        })
+      }
     },
     handler: deleteUser
   },
